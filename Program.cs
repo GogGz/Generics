88fd8b966e8f6f29a4f0248191ce3c6b7e5b3503
GogGz <120939822+GogGz@users.noreply.github.com>
2023-01-13 19:08:50 +0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,24 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
+            Console.WriteLine("__________ INT ___________");
 
             Gen<int> MyGen = new Gen<int>();
 
             int[] array = MyGen.CreateArr(6); // create array
-            Console.Write("Create array   -----  ");
             MyGen.ShowArr(array);
 
             Console.WriteLine();
 
             int[] AddInArray = MyGen.AddIteminArr(array, 8);// add item in array
-            Console.Write("Add item in array   -----  ");
-
             MyGen.ShowArr(AddInArray);
 
             Console.WriteLine();
 
             int[] AddAgain = MyGen.AddIteminArr(AddInArray, 15); // add again
-            Console.Write("Add item in array again   -----  ");
             MyGen.ShowArr(AddAgain);
 
 
@@ -29,8 +26,46 @@ class Program
 
 
             int[] DeleteInArray = MyGen.DeleteIteminArr(AddAgain, 1); // Delete item
-            Console.Write("Delete item from  array   -----  ");
             MyGen.ShowArr(DeleteInArray);
+
+
+            Console.WriteLine("\n\n__________ CHAR ___________");
+
+            Gen<char> CharGen = new Gen<char>(); // ok a vor im popokhakani mej pahum em Gen classi arr
+            char[] char_arr1 = CharGen.CreateArr('a');
+            CharGen.ShowArr(char_arr1);
+            Console.WriteLine();
+
+            char[] char_arr2 = CharGen.AddIteminArr(char_arr1, 'b');
+            CharGen.ShowArr(char_arr2);
+            Console.WriteLine();
+
+            char[] char_arr3 = CharGen.AddIteminArr(char_arr2, 'c');
+            CharGen.ShowArr(char_arr3);
+            Console.WriteLine();
+
+            char[] char_arr4 = CharGen.DeleteIteminArr(char_arr3, 0);
+            CharGen.ShowArr(char_arr4);
+            Console.WriteLine();
+
+            Console.WriteLine("\n\n__________ STRING ___________");
+
+            Gen<string> StrGen = new Gen<string>();
+            string[] str_arr1 = StrGen.CreateArr("Gohar ");
+            StrGen.ShowArr(str_arr1);
+            Console.WriteLine();
+
+            string[] str_arr2 = StrGen.AddIteminArr(str_arr1,"Zhanna ");
+            StrGen.ShowArr(str_arr2);
+            Console.WriteLine();
+
+            string[] str_arr3 = StrGen.AddIteminArr(str_arr2, "Hasmik ");
+            StrGen.ShowArr(str_arr3);
+            Console.WriteLine();
+
+            string[] str_arr4 = StrGen.DeleteIteminArr(str_arr3, 0);
+            StrGen.ShowArr(str_arr4);
+            Console.WriteLine();
 
         }
     }
@@ -93,7 +128,7 @@ class Program
             {
                 Console.WriteLine(e.Message + "\nPlease try again");
             }
-            return CopyArr;
+            return CopyArr;  // vonc anem vor exceptioni depqum eli return chani
         }
 
         public void ShowArr(T[] arr)
